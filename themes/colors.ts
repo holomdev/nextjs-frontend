de1f5d083e2eebe2923c02338e10afbc57d6d12a
holomdev <holom.dev@gmail.com>
2023-06-11 14:54:38 -0600

    add colors

---
+++ themes/colors.ts
@@ -0,0 +1,86 @@
+export const white = '#FFFFFF'
+export const black = '#1E1D2A'
+
+export const neutral = {
+  50: '#FBFAFB',
+  100: '#F4F4F4',
+  200: '#E8E8EA',
+  300: '#D2D2D4',
+  400: '#BBBBBF',
+  500: '#A5A5AA',
+  600: '#8E8E94',
+  700: '#78777F',
+  800: '#62616A',
+  900: '#4B4A55',
+  950: '#35343F'
+}
+
+export const manuals = {
+  50: '#F6F9FF',
+  100: '#ECF3FE',
+  200: '#DAE7FE',
+  300: '#C7DBFD',
+  400: '#B5CFFD',
+  500: '#A2C3FC',
+  600: '#90B7FB',
+  700: '#7EABFB',
+  800: '#6B9FFA',
+  900: '#5993FA',
+  950: '#4687F9'
+}
+
+export const orange = {
+  50: '#FFF7F2',
+  100: '#FFEFE5',
+  200: '#FFDECC',
+  300: '#FFCEB2',
+  400: '#FFBE99',
+  500: '#FFAD80',
+  600: '#FF9D66',
+  700: '#FF8D4D',
+  800: '#FF7C33',
+  900: '#FF6C1A',
+  950: '#FF5C00'
+}
+
+export const purple = {
+  50: '#F9F6FF',
+  100: '#F3EDFE',
+  200: '#E8DAFD',
+  300: '#DCC8FD',
+  400: '#D1B6FC',
+  500: '#C5A3FB',
+  600: '#B991FA',
+  700: '#AE7FF9',
+  800: '#A26DF9',
+  900: '#975AF8',
+  950: '#8B48F7'
+}
+
+export const error = {
+  50: '#FFF2F5',
+  100: '#FFE5EB',
+  200: '#FFCCD7',
+  300: '#FFB2C2',
+  400: '#FF99AE',
+  500: '#FF809A',
+  600: '#FF6686',
+  700: '#FF4D72',
+  800: '#FF335D',
+  900: '#FF1A49',
+  950: '#FF0035'
+}
+
+export const success = {
+  50: '#F4FBF5',
+  100: '#E9F7EA',
+  200: '#D2EFD5',
+  300: '#BCE6C0',
+  400: '#A6DEAB',
+  500: '#8FD696',
+  600: '#79CE82',
+  700: '#63C66D',
+  800: '#4DBD58',
+  900: '#20AD2E',
+  950: '#20AD2E'
+}
