051e16724d42c32497c95e70a72001e13c9172d8
holomdev <holom.dev@gmail.com>
2023-06-11 14:52:35 -0600

    add mui theme global config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider, CssBaseline } from '@mui/material'
+import { Theme } from '@/themes/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <ThemeProvider theme={Theme}>
+    <CssBaseline />
+    <Component {...pageProps} />
+  </ThemeProvider>)
 }
