01778f5ad7c54b72d5a80284da1cc107a003138b
holomdev <holom.dev@gmail.com>
2023-06-14 13:33:53 -0600

    add fetch request example

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,9 +1,23 @@
 import { AppBar, Box, Button, IconButton, Toolbar, Typography } from "@mui/material"
 import MenuIcon from '@mui/icons-material/Menu';
 import { useSession, signOut } from "next-auth/react";
+import { useState } from "react";
 
 const HomePage = () => {
   const { data: session } = useSession()
+  const [data, setData] = useState()
+
+  const fetchGet = async () => {
+    const res = await fetch("http://localhost:3500/coffees", {
+      method: "Get",
+      headers: {
+        authorization: `bearer ${session?.user.accessToken}`
+      }
+    })
+
+    const response = await res.json()
+    setData(response)
+  }
 
   return (
     <>
@@ -20,11 +34,20 @@ const HomePage = () => {
               <MenuIcon />
             </IconButton>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Hola, {session?.user?.name} 
+              Hola, {session?.user?.name}
             </Typography>
             <Button color="inherit" onClick={() => signOut()}>Logout</Button>
           </Toolbar>
         </AppBar>
+      </Box>
+      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Box width="500px" sx={{ display: 'flex', flexDirection: 'column' }}>
+          <Typography py={3}>Peticion al backend</Typography>
+          <Button variant="contained" onClick={fetchGet}>Enviar peticion</Button>
+          <Box>
+            {data && JSON.stringify(data)}
+          </Box>
+        </Box>
       </Box>
     </>
   )
