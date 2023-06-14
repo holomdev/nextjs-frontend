7c0bac5fbc0fa66a098c200b510b1da76c9457e5
holomdev <holom.dev@gmail.com>
2023-06-14 15:39:53 -0600

    add axios auth to request backend

--- lib/axios.ts
+++ lib/axios.ts
@@ -6,3 +6,8 @@ export default axios.create({
   baseURL: BASE_URL,
   headers: { "Content-Type": "application/json" }
 })
+
+export const axiosAuth = axios.create({
+  baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json" }
+})
--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -2,21 +2,16 @@ import { AppBar, Box, Button, IconButton, Toolbar, Typography } from "@mui/mater
 import MenuIcon from '@mui/icons-material/Menu';
 import { useSession, signOut } from "next-auth/react";
 import { useState } from "react";
+import useAxiosAuth from "@/lib/hooks/useAxiosAuth";
 
 const HomePage = () => {
   const { data: session } = useSession()
   const [data, setData] = useState()
+  const axiosAuth = useAxiosAuth()
 
   const fetchGet = async () => {
-    const res = await fetch("http://localhost:3500/coffees", {
-      method: "Get",
-      headers: {
-        authorization: `bearer ${session?.user.accessToken}`
-      }
-    })
-
-    const response = await res.json()
-    setData(response)
+    const res = await axiosAuth.get("/coffees")
+    setData(res.data)
   }
 
   return (
